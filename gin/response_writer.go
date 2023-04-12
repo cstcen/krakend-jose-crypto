f2541ecb98b9ff32e9d30994016d3aa9b35c6947
Chester <chestercen@tenvine.cn>
2023-04-12 17:25:54 +0800

    feat(*): add log

--- gin/response_writer.go
+++ gin/response_writer.go
@@ -1,44 +1,43 @@
 package gin
 
 import (
-    "encoding/json"
-    "github.com/gin-gonic/gin"
-    jose "github.com/krakendio/krakend-jose/v2"
-    "github.com/luraproject/lura/v2/logging"
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	jose "github.com/krakendio/krakend-jose/v2"
+	"github.com/luraproject/lura/v2/logging"
 )
 
 type ResponseWriter struct {
-    gin.ResponseWriter
-    jose.SignerConfig
-    logger    logging.Logger
-    logPrefix string
+	gin.ResponseWriter
+	jose.SignerConfig
+	logger    logging.Logger
+	logPrefix string
 }
 
 func (r *ResponseWriter) Write(p []byte) (n int, err error) {
-    var res map[string]any
-    if err := json.Unmarshal(p, &res); err != nil {
-        return r.ResponseWriter.Write(p)
-    }
-    for _, k := range r.KeysToSign {
-        tmp, ok := res[k].(string)
-        if !ok {
-            continue
-        }
+	var res map[string]any
+	if err := json.Unmarshal(p, &res); err != nil {
+		return r.ResponseWriter.Write(p)
+	}
+	for _, k := range r.KeysToSign {
+		tmp, ok := res[k].(string)
+		if !ok {
+			r.logger.Warning(r.logPrefix, "failed to convert to string, key: ", k)
+			continue
+		}
 
-        ciphertext, err := CBCEncrypt(tmp, r.CipherKey)
-        if err != nil {
-            r.logger.Warning(r.logPrefix, "failed to cbc encrypt: ", err.Error())
-            continue
-        }
+		ciphertext, err := CBCEncrypt(tmp, r.CipherKey)
+		if err != nil {
+			r.logger.Warning(r.logPrefix, "failed to cbc encrypt: ", err.Error())
+			continue
+		}
 
-        res[k] = ciphertext
-    }
-    r.logger.Debug(r.logPrefix, " before Write: ", res)
-    raw, err := json.Marshal(res)
-    if err != nil {
-        return r.ResponseWriter.Write(p)
-    }
-    r.logger.Debug(r.logPrefix, " after Write: ", string(raw))
-    return r.ResponseWriter.Write(raw)
+		res[k] = ciphertext
+	}
+	r.logger.Debug(r.logPrefix, " Write: ", res)
+	raw, err := json.Marshal(res)
+	if err != nil {
+		return r.ResponseWriter.Write(p)
+	}
+	return r.ResponseWriter.Write(raw)
 }
-
